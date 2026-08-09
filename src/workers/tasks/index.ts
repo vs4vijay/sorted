66dc446e6db596f5d8428f6f3b0eba336c9c7796
vs4vijay <vs4vijay@gmail.com>
2026-08-09 23:45:18 +0530

    Recruiting

--- src/workers/tasks/index.ts
+++ src/workers/tasks/index.ts
@@ -3,10 +3,12 @@ import processItemTask from './process-item';
 import sendNotificationTask from './send-notification';
 import extractCvDocumentTask from './extract-cv-document';
 import sendOutreachFollowupTask from './send-outreach-followup';
+import enforceRetentionPolicyTask from './enforce-retention-policy';
 
 export function registerAllTasks(worker: Worker): void {
   worker.registerTask('process-item', processItemTask);
   worker.registerTask('send-notification', sendNotificationTask);
   worker.registerTask('extract-cv-document', extractCvDocumentTask);
   worker.registerTask('send-outreach-followup', sendOutreachFollowupTask);
+  worker.registerTask('enforce-retention-policy', enforceRetentionPolicyTask);
 }
